fc3278e41c58b32759ee5e6183883baa5cb6f772
Esmé <esmealicejames@gmail.com>
2018-03-03 14:55:23 -0500

    Dialogues in cell scene

---
+++ Assets/Scripts/UI/DialogueManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public Text nomPnj, question;
+    public List<Button> reponses;
+    public Button endConv;
+    public GameObject panel;
+
+    private Interaction m_qCourante, m_qSuivante;
+    private List<Interaction> qList;
+    private List<Rep> reponsesQuestion;
+
+
+    public List<Interaction> GetDialogue(int niveau, int pnj, int personnalite) {
+
+        string idDico = niveau.ToString() + "." + pnj.ToString() + "." + personnalite.ToString();
+        Conv conv = Dialogues.GetConv(idDico);
+        List<Interaction> qList = conv.interactionListe;
+        
+        return qList;
+    }
+
+    public void Start() {
+
+        //qList = GetDialogue(0, 2, 1);
+
+        ////if(player.transform.position < certaineDistance && mouseClick && !hasSpokento)
+        //// {
+
+        //qCourante = qList[0];
+        //question.text = qCourante.interaction;
+        //reponsesQuestion = qCourante.reponseListe;
+
+        //AfficheDialogue();
+    }
+
+    public void setButtonQuestion(string text)
+    {
+        question.text = text;
+    }
+
+    public void GetAnswerPressed(int idReponse)
+    {
+        int idQSuivante = reponsesQuestion[idReponse].interacSuiv;
+
+        foreach (Interaction q in qList)
+        {
+            if (q.id == idQSuivante) m_qSuivante = q;
+        }
+        m_qCourante = m_qSuivante;
+
+        AfficheDialogue(m_qCourante, qList);
+    }
+
+    public void AfficheDialogue(Interaction qCourante, List<Interaction> m_qList)
+    {
+        qList = m_qList;
+        question.text = qCourante.interaction;
+        reponsesQuestion = qCourante.reponseListe;
+
+        int i = 0;
+        if (reponsesQuestion != null)
+        {
+
+            foreach (Rep rep in reponsesQuestion)
+            {
+                reponses[i].GetComponentInChildren<Text>().text = rep.reponse;
+                i++;
+            }
+        } else
+        {
+            foreach (Button b in reponses)
+            {
+                b.gameObject.SetActive(false);
+            }
+
+            endConv.gameObject.SetActive(true);
+        }
+
+    }
+
+    public void EndConversation()
+    {
+        panel.SetActive(false);
+    }
+
+   
+}
---
+++ Assets/Scripts/UI/Dialogues.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conv {
+    public List<Interaction> interactionListe;
+}
+
+public class Interaction {
+    public int id;
+    public string interaction;
+    public List<Rep> reponseListe;
+}
+
+public class Rep {
+    public string reponse;
+    public int interacSuiv;
+    public int soupcon;
+}
+
+public static class Dialogues {
+
+    private static Dictionary<string, Conv> _conv = new Dictionary<string, Conv> {
+        //**************************************************************************************
+        // NIVEAU 0 (tutoriel)
+        //**************************************************************************************
+        //Conversation (niveau 0, pnj 1, personalité 1)
+        {"0.1.1", new Conv { interactionListe = new List<Interaction>() {
+            new Interaction { id = 1, interaction = "Salut Ernest… ou Jack, ou…je ne sais quel prénom tu préfères aujourd’hui. Tu vas bien ?", reponseListe = new List<Rep>() {
+                new Rep { reponse = "... ça pourrait aller mieux vu l’endroit où on est. Écoute, je cherche à foutre le camp d’ici. T’aurais pas un petit contact qui m’aiderait ?", interacSuiv = 2, soupcon = 0 },
+                new Rep { reponse = "... Je vais toujours bien moi. Mais j'ai besoin de ton aide pour partir d'ici. Tu connais quelqu'un non ?", interacSuiv = 2, soupcon = 0 },
+                new Rep { reponse = "... Non ça va pas, faut que je parte d'ici. T'aurais pas un contact pour m'aider ?", interacSuiv = 2, soupcon = 0 }
+                }
+            },
+            new Interaction { id = 2, interaction = "Ouais ouais je connais quelqu’un. Ya Marco qui traîne souvent dans la salle commune… Va le voir, il connaît bien cet étage. Mais fait gaffe d’être poli si tu veux des réponses, il aime pas être contrarié ce gars là.", reponseListe = null }
+        } } },
+
+        //Conversation (niveau 0, pnj 2, personalité 1)
+        {"0.2.1", new Conv { interactionListe = new List<Interaction>() {
+            new Interaction { id = 1, interaction = "On s'connaît ?", reponseListe = new List<Rep>() {
+                new Rep { reponse = "Bonjour Marco! Moi c’est Ernest, content de te rencontrer. Fred m’a dit que tu pourrais peut-être m’aider.", interacSuiv = 2, soupcon = 0 },
+                new Rep { reponse = "Pas encore. Moi c’est Ernest, c’est ton ami Fred qui m’a dit de venir te voir pour un service.", interacSuiv = 3, soupcon = 0 },
+                new Rep { reponse = "Non, mais j’aurais un petit service à te demander.", interacSuiv = 4, soupcon = 0 }
+                }
+            },
+            new Interaction { id = 2, interaction = "Ah, ça fait du bien d’entendre un peu de politesse par ici!  Qu’est ce qu’il te faut mon ami ?", reponseListe = new List<Rep>() {
+                new Rep { reponse = "Je cherche à m’évader d’ici mais je ne sais pas par où commencer...", interacSuiv = 5, soupcon = 0 },
+                new Rep { reponse = "Faut que j’me barre, j’ai rien à foutre dans cet asile moi. Je suis sain d’esprit, sain d'esprit, tu vois. Tu peux m’aider ?", interacSuiv = 5, soupcon = 0 },
+                new Rep { reponse = "Faut que j'mévade !", interacSuiv = 5, soupcon = 0 }
+                }
+            },
+            new Interaction { id = 3, interaction = "Un ami de Fred est un ami à moi! Qu’est ce qu’il de faut ?", reponseListe = new List<Rep>() {
+                new Rep { reponse = "Je cherche à m’évader d’ici mais je ne sais pas par où commencer...", interacSuiv = 6, soupcon = 0 },
+                new Rep { reponse = "Faut que j’me barre, j’ai rien à foutre dans cet asile moi. Je suis sain d’esprit, sain d'esprit, tu vois. Tu peux m’aider ?", interacSuiv = 6, soupcon = 0 },
+                new Rep { reponse = "Faut que j'mévade !", interacSuiv = 6, soupcon = 0 }
+                }
+            },
+            new Interaction { id = 4, interaction = "Un bonjour ne ferait pas de mal. Qu’es tu veux ?", reponseListe = new List<Rep>() {
+                new Rep { reponse = "Je cherche à m’évader d’ici mais je ne sais pas par où commencer...", interacSuiv = 7, soupcon = 0 },
+                new Rep { reponse = "Faut que j’me barre, j’ai rien à foutre dans cet asile moi. Je suis sain d’esprit, sain d'esprit, tu vois. Tu peux m’aider ?", interacSuiv = 7, soupcon = 0 },
+                new Rep { reponse = "Faut que j'mévade !", interacSuiv = 7, soupcon = 0 }
+                }
+            },
+            new Interaction { id = 5, interaction = "J’connais l’asile comme ma poche mon pote. Si tu veux t’évader, commence par rentrer dans la salle de pause des médecins, ils ont des clés qui donnent accès à pas mal d’endroits. Mais attention, ils ne font pas confiance à tout le monde... ", reponseListe = null },
+            new Interaction { id = 6, interaction = "T’as de la chance, je connais bien l’asile. Va voir dans le coin de pause des médecins, ils peuvent t’ouvrir pas mal de portes par ici.", reponseListe = null },
+            new Interaction { id = 7, interaction = "Regarde dans les salles de ce couloir, j’entends que les médecins sont en pause. Et reviens surtout pas me voir.", reponseListe = null }
+        } } },
+
+        //Conversation (niveau 0, pnj 3, personalité 2)
+        {"0.3.2", new Conv { interactionListe = new List<Interaction>() {
+            new Interaction { id = 1, interaction = "Bonjour collègue. Comment allez-vous aujourd’hui ?", reponseListe = new List<Rep>() {
+                new Rep { reponse = "Pas terrible à vrai dire… Un patient vient tout juste de me faire un croche-patte alors que je me baladais dans le couloir...", interacSuiv = 2, soupcon = 0 }
+                }
+            },
+            new Interaction { id = 2, interaction = "Quoi ?! Ce genre de comportement est intolérable ! Prenez les clés sur la table et allez voir les gardiens dans leur bureau au bout du couloir pour leur en faire part.", reponseListe = null }
+        } } }
+
+        //**************************************************************************************
+        // NIVEAU 1
+        //**************************************************************************************
+    };
+
+    public static Conv GetConv(string id) {
+        Conv text;
+        _conv.TryGetValue(id, out text);
+        return text;
+    }
+}
