1d383aba86e58b9e94e293ff04bd982f3dc0835e
Sacha vanleen <sacha1231@hotmail.fr>
2018-03-03 01:42:57 -0500

    ajout guard et player

---
+++ Assets/Scripts/IA/Guard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : MonoBehaviour {
+
+
+
+    public int suspicion;
+
+
+    private void Awake()
+    {
+        suspicion = 0;
+    }
+
+
+    public void IncreaseSuspission(int s)
+    {
+        suspicion += s;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
