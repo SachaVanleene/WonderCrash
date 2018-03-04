30dab6dd94d45cf414821bff2d85ae7f8fe48b42
Ayoub-AH <aub.al.h@gmail.com>
2018-03-03 23:54:52 -0500

    tuto

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -77,7 +77,7 @@ public class PlayerStats : MonoBehaviour {
         }
         if(currentSoupcon >= maxSoupconLevel && !spotted)
         {
-            spotted = true;
+            setSpotted(true);
         }
 
 	}
