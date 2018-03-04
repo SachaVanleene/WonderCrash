9ed0763dbfd1fc4aabc63d6fc97def31d40735c0
JulienQC <julien.cusset33@gmail.com>
2018-03-03 20:48:02 -0500

    ajout de repulsive wall

---
+++ Assets/Scripts/Player/RepulsiveWall.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepulsiveWall : MonoBehaviour {
+
+    public Vector3 force;
+
+    private Collider wallCollider;
+    private PlayerController player;
+    private float playerSpeed;
+
+	// Use this for initialization
+	void Start () {
+        wallCollider = GetComponent<Collider>();
+        player = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<PlayerController>();
+        playerSpeed = player.speed;
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        GameObject obj = other.gameObject;
+        if (obj.CompareTag("Player"))
+        {
+            if(force != null)
+            {
+                player.speed = 0;
+                obj.GetComponent<Rigidbody>().AddForce(force, ForceMode.VelocityChange);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GameObject obj = other.gameObject;
+        if (obj.CompareTag("Player"))
+        {
+            //clear all forces
+            obj.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            obj.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            player.speed = playerSpeed;
+        }
+    }
+
+}
