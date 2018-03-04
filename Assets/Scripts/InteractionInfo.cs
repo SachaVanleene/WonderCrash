3bd5fe3fc938174445d18a00a2a03c0dad72709e
Sacha vanleen <sacha1231@hotmail.fr>
2018-03-03 20:03:03 -0500

    On essaie d'avoir une scene qui marche

---
+++ Assets/Scripts/InteractionInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionInfo : MonoBehaviour
+{
+    public GameObject panelInfo;
+
+    private void Start()
+    {
+        //panelInfo = GameObject.FindGameObjectWithTag("InteractionPanel");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 12)
+        {
+            panelInfo.SetActive(true);
+            panelInfo.GetComponentInChildren<Text>().text = "appuyer sur E pour interagir";
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 12)
+        {
+            panelInfo.SetActive(false);
+            panelInfo.GetComponentInChildren<Text>().text = "";
+        }
+    }
+}
